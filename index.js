63dff72d9e60fb5982f558e9171b1e618ce2eb41
Max <max.zhao89@gmail.com>
2021-07-05 14:58:12 -0700

    added the functionality of handling orders with multiple items. Including items from the same seller.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.json())
 app.use(express.static("C:/Rerouted/views/styles.css"));
 
-const link = 'https://752fa5ab548b2853ffff1fdccc5e1be2:c06eaec5f0217effc29509fe2f4d90a5@rerouted-co-op.myshopify.com/admin/api/2021-04/orders.json'; //shopify 'order' api key. Use fetch to access
+//const link = 'https://752fa5ab548b2853ffff1fdccc5e1be2:c06eaec5f0217effc29509fe2f4d90a5@rerouted-co-op.myshopify.com/admin/api/2021-04/orders.json'; //shopify 'order' api key. Use fetch to access
 
 app.listen(3000, () => {console.log('app listening on port 3000')}); 
 
@@ -35,16 +35,10 @@ async function sendEmail(data) {
             console.log(err);
         } else {
             var mainOptions = {
-                from: 'max@rerouted.co',
+                from: 'support@rerouted.co',
                 to: 'max@rerouted.co',
                 // to: 'data.sellerEmail',
                 subject: "Congrats! You've sold an item on Rerouted!",
-                // attachments: [{
-                //     filename: 'rerouted-compact-RGB-reverse-black-large.png',
-                //     path: 'C:/Rerouted/views/rerouted-compact-RGB-reverse-black-large.png',
-                //     content: fs.createReadStream('C:/Rerouted/views/rerouted-compact-RGB-reverse-black-large.png'),
-                //     //cid: 'unique@nodemailer.com'
-                // }],
                 html: ejs //ejs template with data injected
             };
             console.log("html data ======================>", mainOptions.html);
@@ -65,33 +59,100 @@ async function sendEmail(data) {
 
 app.post('/webhook/order', async (req, res) => {
     const body = await req.body;
+    var lineItems = body.line_items;
     const customer = body.customer;
     const customerAddress = body.billing_address;
-    
-    var data = { //sets all the important info we need to inject into the email template
-        name: customer.first_name,
-        street: customerAddress.address1,
-        city: customerAddress.city,
-        state: customerAddress.province,
-        zip: customerAddress.zip,
-        customerEmail: customer.email,
-        sellerEmail: body.line_items.vendor,
-        address2: ''
-    };
 
-    if (customerAddress.address2 != "") {data.address2 = customerAddress.address2;} //sets an address2 if there is an address2
+    //console.log(body.line_items);
+    if (lineItems.length = 1) //if there is only one item this order... then we just send one email by calling the sendEmail function once
+    {
+        var data = { //sets all the important info we need to inject into the email template
+            name: customer.first_name,
+            street: customerAddress.address1,
+            city: customerAddress.city,
+            state: customerAddress.province,
+            zip: customerAddress.zip,
+            customerEmail: customer.email,
+            sellerEmail: lineItems.vendor,
+            address2: '',
+            itemName: lineItems[i].name
+        };
 
-    res.render('index.ejs', data)
+        if (customerAddress.address2 != "") {data.address2 = customerAddress.address2;} //sets an address2 if there is an address2        
+        
+        sendEmail(data); //passess data from shopify to our email template to send
+    }
 
-    //res.status(200).send('post request received').end();
-    
-    sendEmail(data);
+    else if (lineItems.length > 1) //if there are more than one item this order...
+    {
+        for (var i = 0; i < lineItems.length; i++) //iterate through each item
+        {
+            var emails = []; //temporary array for items with the same seller
+            var duplicate = false; //boolean for whether this item's seller sold another item within this order
+            for (var j = i + 1; j < lineItems.length; j++) //iterates through rest of items this order to compare to the initial item
+            {
+                if (lineItems[i].vendor == lineItems[j].vendor) //if the two items are from the same seller...
+                {
+                    duplicate = true;
+                    emails.push(j); //adding same seller items to temporary array
+                }
+            }
 
+            if (duplicate) //process for handling data if this item shares the same seller with other items in this order
+            {
+                emails.push(i); //adding same seller items to temp array
+                var str = emails[0]; 
+                for (var k = 1; k < emails.length; k++) //linking together item names for itemName property
+                {
+                    str += ` and your ${emails[k]}`;  
+                }
+                var data = { //sets all the important info we need to inject into the email template
+                    name: customer.first_name,
+                    street: customerAddress.address1,
+                    city: customerAddress.city,
+                    state: customerAddress.province,
+                    zip: customerAddress.zip,
+                    customerEmail: customer.email,
+                    sellerEmail: lineItems[i].vendor, //only sends one email to this seller
+                    address2: '',
+                    itemName: str 
+                };
+        
+                if (customerAddress.address2 != "") {data.address2 = customerAddress.address2;} //sets an address2 if there is an address2        
+                
+                sendEmail(data);
+                for (var l = 0; l < emails.length; l++) //deleting all items with same seller so that they aren't detected later in the loop
+                {
+                    delete lineItems[l];  
+                }
+            }
+
+            else //process for handling data if this item has a unique seller
+            {   
+                var data = { //sets all the important info we need to inject into the email template
+                    name: customer.first_name,
+                    street: customerAddress.address1,
+                    city: customerAddress.city,
+                    state: customerAddress.province,
+                    zip: customerAddress.zip,
+                    customerEmail: customer.email,
+                    sellerEmail: lineItems[i].vendor,
+                    address2: '',
+                    itemName: lineItems[i].name
+                };
+        
+                if (customerAddress.address2 != "") {data.address2 = customerAddress.address2;} //sets an address2 if there is an address2        
+                
+                sendEmail(data); 
+                delete lineItems[i];  
+            }
+
+
+        }
+    }
 })
 
 /*Current problems/questions:
-How do we create a new html <p> for address 2 if there is one?
-image/attachments not working with nodemail but I suspect that has to do with my localhost server not being able to handle it
 What's the host if we're running the app on our own web server?
 */
 
@@ -100,3 +161,108 @@ What's the host if we're running the app on our own web server?
 
 
 
+
+//testing!!
+
+app.get('/', async (req, res) => {
+    const link = 'https://752fa5ab548b2853ffff1fdccc5e1be2:c06eaec5f0217effc29509fe2f4d90a5@rerouted-co-op.myshopify.com/admin/api/2021-04/orders.json';
+    const json = await fetch(link);
+    var x = await json.json();
+    const orders = x.orders;
+    const body = orders[13];
+    //const body = await req.body;
+    //console.log(orders[13].line_items);
+    const lineItems = body.line_items;
+    //console.log(lineItems);
+    
+    const customer = body.customer;
+    const customerAddress = body.billing_address;
+
+    if (lineItems.length == 1) //if there is only one item this order... then we just send one email by calling the sendEmail function once
+    {
+        var data = { //sets all the important info we need to inject into the email template
+            name: customer.first_name,
+            street: customerAddress.address1,
+            city: customerAddress.city,
+            state: customerAddress.province,
+            zip: customerAddress.zip,
+            customerEmail: customer.email,
+            sellerEmail: lineItems.vendor,
+            address2: '',
+            itemName: lineItems[0].name
+        };
+
+        if (customerAddress.address2 != "") {data.address2 = customerAddress.address2;} //sets an address2 if there is an address2        
+        
+        sendEmail(data); //passess data from shopify to our email template to send
+    }
+    
+    else if (lineItems.length > 1) //if there are more than one item this order...
+    {
+        for (var i = 0; i < lineItems.length; i++) //iterate through each item
+        {
+            var emails = []; //temporary array for items with the same seller
+            var duplicate = false; //boolean for whether this item's seller sold another item within this order
+            for (var j = i + 1; j < lineItems.length; j++) //iterates through rest of items this order to compare to the initial item
+            {
+                if (lineItems[i].vendor == lineItems[j].vendor) //if the two items are from the same seller...
+                {
+                    duplicate = true;
+                    emails.push(j); //adding same seller items to temporary array
+                }
+            }
+
+            if (duplicate) //process for handling data if this item shares the same seller with other items in this order
+            {
+                emails.push(i); //adding same seller items to temp array
+                var str = emails[0]; 
+                for (var k = 1; k < emails.length; k++) //linking together item names for itemName property
+                {
+                    str += ` and your ${emails[k]}`;  
+                }
+                var data = { //sets all the important info we need to inject into the email template
+                    name: customer.first_name,
+                    street: customerAddress.address1,
+                    city: customerAddress.city,
+                    state: customerAddress.province,
+                    zip: customerAddress.zip,
+                    customerEmail: customer.email,
+                    sellerEmail: lineItems[i].vendor, //only sends one email to this seller
+                    address2: '',
+                    itemName: str 
+                };
+        
+                if (customerAddress.address2 != "") {data.address2 = customerAddress.address2;} //sets an address2 if there is an address2        
+                
+                sendEmail(data);
+            }
+
+            else //process for handling data if this item has a unique seller
+            {   
+                var data = { //sets all the important info we need to inject into the email template
+                    name: customer.first_name,
+                    street: customerAddress.address1,
+                    city: customerAddress.city,
+                    state: customerAddress.province,
+                    zip: customerAddress.zip,
+                    customerEmail: customer.email,
+                    sellerEmail: lineItems[i].vendor,
+                    address2: '',
+                    itemName: lineItems[i].name
+                };
+        
+                if (customerAddress.address2 != "") {data.address2 = customerAddress.address2;} //sets an address2 if there is an address2        
+                
+                sendEmail(data);   
+            }
+
+
+        }
+    }
+    //console.log(lineItems);
+})
+
+/*Current problems/questions:
+What's the host if we're running the app on our own web server?
+Very rare case of email property being null
+*/
